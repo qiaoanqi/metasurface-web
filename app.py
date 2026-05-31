f6070fe84622c0c9dfad73219cf7143b40da029c
qiaoanqi <2197893158@qq.com>
2026-06-01 03:29:14 +0800

    TEST: force red color when angle > 0

--- app.py
+++ app.py
@@ -249,6 +249,14 @@ class MetaSurfaceColorEngine:
         # Single dominant Lorentzian-like resonance
         amp = 1.0 / (1.0 + ((wl_nm - (lam_peak + angle_shift))/sigma)**2)
 
+                # --- TEST: force red when angle > 0 ---
+        if param.angle_deg > 0.5:
+            wl_nm_norm = (wl_nm - 380) / 400.0
+            if wl_nm > 600:
+                return 1.0
+            else:
+                return 0.01
+        # --- END TEST ---
         return float(amp * (0.30 + 0.80*fill) * loss * amp_angle)
 
     def compute_spectrum(self, param, wl_start=380.0, wl_end=780.0, n_pts=81):
